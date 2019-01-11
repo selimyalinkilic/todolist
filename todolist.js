39a7fff320f691cf8cbffab0ca75ea0133db9b3f
selimyalinkilic <yalinkilicselim@gmail.com>
2019-01-11 17:23:27 +0300

    Updated
    Updated

--- todolist.js
+++ todolist.js
@@ -7,17 +7,7 @@ let todolist = (function () {
 
   function Todo(name, date){ // Todo's items 
     this.name = name;
-    this.name = name;
-    let today = new Date();
-    let dd = today.getDate();
-
-    let mm = today.getMonth()+1; 
-    let yyyy = today.getFullYear();
-    if(dd<10)dd='0'+dd;
-
-    if(mm<10)mm='0'+mm;
-    today = dd+'/'+mm+'/'+yyyy;
-    this.date = today;
+    this.date = date;
   }
   function saveTodos() { // Saving Todos to localstorage
     localStorage.setItem("todolist", JSON.stringify(todos));
